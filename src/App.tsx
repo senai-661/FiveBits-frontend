69ab0c76029ff183479bf7aa04b7abb2ee7e3bc2
Lívia Borges Matos <livia.matos2721@gmail.com>
2026-04-22 14:57:35 -0300

    feat: renderização condicional e proteção das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import PLogin from './pages/PLogin/Plogin'
 import PListagemPaciente from './components/Listagens/ListagemPaciente/ListagemPaciente'
 import PListagemMedico from './components/Listagens/ListagemMedico/ListagemMedico'
 import PListagemConsulta from './components/Listagens/ListagemConsulta/ListagemConsulta'
+import ProtectedRoute from './components/Rotas/ProtectedRotes'
 
 
 function App() {
@@ -14,10 +15,10 @@ function App() {
       <Routes>
         {/* Rota Principal: Landing Page do MedFlow */}
         <Route path='/' element={<PHome />} /> 
-        <Route path='/lista/Login' element = {<PLogin/>}/>
-        <Route path='/lista/paciente' element={<PListagemPaciente />}/>
-        <Route path='/lista/medico' element={<PListagemMedico />}/>
-        <Route path='/lista/consulta' element={<PListagemConsulta />}/> 
+        <Route path='/login' element = {<PLogin/>}/>
+        <Route path='/lista/paciente' element={<ProtectedRoute element={<PListagemPaciente />} />}/>
+        <Route path='/lista/medico' element={<ProtectedRoute element={<PListagemMedico />} />}/>
+        <Route path='/lista/consulta' element={<ProtectedRoute element={<PListagemConsulta />} />}/> 
 
       </Routes>
     </BrowserRouter>
@@ -25,3 +26,5 @@ function App() {
 }
 
 export default App;
+
+
