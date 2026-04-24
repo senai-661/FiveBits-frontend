9067855577514ed5aa88f84b4ed869973e072e26
Lívia Borges Matos <livia.matos2721@gmail.com>
2026-04-24 08:51:35 -0300

    refactor: refatação do código para a listagem do nome do paciente e médico serem listados na tabela consulta

--- src/dto/ConsultaDTO.ts
+++ src/dto/ConsultaDTO.ts
@@ -1,10 +1,26 @@
 export interface ConsultaDTO {
-    idConsulta: number;
-    idPaciente?: number; // Opcional
-    idMedico?: number; // Opcional
+       idConsulta?: number;
     dataHora: Date;
-    status?: string; // Opcional
-    modalidade: string;
+    status?: 'Pendente' | 'Confirmado' | 'Cancelado' | 'Concluido';
+    modalidade?: 'Pessoalmente' | 'Telemedicina';
     triagemSintomas: string;
-    situacao?: boolean; // Opcional
+    situacao?: boolean;
+
+    paciente: {
+        idPaciente: number;
+        nomePaciente: string;
+        cpf: string;
+        telefone?: string;
+        dataNascimento: Date;
+        situacao?: boolean;
+    }
+
+    medico: {
+        idMedico: number;
+        nomeMedico: string;
+        crm: string;
+        especialidade: string;
+        valorConsulta: number;
+        situacao?: boolean;
+    }
 }
